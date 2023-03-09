e474f845c8509b78fbeb1e9cbc4c0c3b10cd8e46
FUELEDBYESPRESSO\leont <xavicano@uw.edu>
2023-03-08 18:28:44 -0800

    Header changes

--- frontend/src/Components/HeaderBar/HeaderBar.tsx
+++ frontend/src/Components/HeaderBar/HeaderBar.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import {Search} from "../Search/Search";
 import {AccountDropdown} from "../AccountDropdown/AccountDropdown";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 export function HeaderBar() {
+    const store = useSelector((state: any) => state.user);
+
     return (
         <div className={"header-bar"}>
             <AccountDropdown/>
             <Search/>
             <div className={"links"}>
-                <Link to={"/"}>Did you watch?</Link>
-                <Link to={"/my-watchlist"}>My Watchlist</Link>
+                <Link className={"logo"} to={"/"}>Did you watch?</Link>
+                {store.userExists &&
+                    <>
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to={"/my-movies"}>My Movies</NavLink>
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to={"/my-shows"}>My Shows</NavLink>
+                        <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to={"/account"}>Account</NavLink>
+                    </>
+                }
             </div>
         </div>
     )
