5a66a146c52e2daaaf25321c0e9168a0e32b4532
FUELEDBYESPRESSO\leont <xavicano@uw.edu>
2023-10-19 12:46:54 -0700

    Changed login navigation

--- api/actors/actors.go
+++ api/actors/actors.go
@@ -2,6 +2,7 @@ package actors
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
@@ -17,6 +18,7 @@ func getActor() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id := c.Param("id")
 		resp, err := http.Get("https://api.themoviedb.org/3/person/" + url.QueryEscape(id) + "?api_key=" + os.Getenv("TMDB_API_KEY") + "&append_to_response=combined_credits")
+		fmt.Println("https://api.themoviedb.org/3/person/" + url.QueryEscape(id) + "?api_key=" + "HIDDEN KEY" + "&append_to_response=combined_credits")
 		if err != nil {
 			return
 		}
